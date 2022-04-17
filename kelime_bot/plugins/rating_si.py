d71c29870246a4450f74004e6f1065af81c5cbd8
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-04-17 14:20:47 +0300

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -18,8 +18,8 @@ async def ratingsa(c:Client, m:Message):
     for puan in puanlar:
         for kisi in rating:
             if puan == rating[kisi]:
-                metin += f"✓ {kisi}  =>\n𝖯𝗎𝖺𝗇 :  {puan}"
-                eklenen += 1
+                metin += f"✓ **{kisi}**  =>\n𝖯𝗎𝖺𝗇 :  **{puan}**\n"
+                eklenen += 0
                 if eklenen == 20:
                     break
                 
