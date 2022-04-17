f13c0a2889ba888438de77ff4746956545be10a0
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-04-17 18:27:38 +0300

    Delete private.py

--- kelime_bot/plugins/private.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-
-message= "**• Merhaba 🎉\n\n• Ben Bir Oyun Botuyum 🎮** \n\n**• Çeşitli oyunlar oynamak ve eğlenceli vakit geçirmek için benimle oynayabilirsin ✍🏻 **\n\n**• Benimle oynamak için beni bir gruba ekleyip yönetici yapman lazim 💭**"
-
-
-@Client.on_message(filters.private & filters.command("start"))
-async def starttgg(c, m):
-    await m.reply(message)
