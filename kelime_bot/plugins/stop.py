0d2201291b5f5b8820981b00089c5558f234ba01
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-04-17 10:04:29 +0300

    Update stop.py

--- kelime_bot/plugins/stop.py
+++ kelime_bot/plugins/stop.py
@@ -15,12 +15,12 @@ async def stop(c:Client, m:Message):
     
     siralama = []
     for i in oyun[m.chat.id]["oyuncular"]:
-        siralama.append(f"{oyun[m.chat.id]['oyuncular'][i]}  :  {i}")
+        siralama.append(f"{i}  :  {oyun[m.chat.id]['oyuncular'][i]}")
     siralama.sort(reverse=True)
     siralama_text = ""
     for i in siralama:
         siralama_text += i + "\n"     
     
-    await c.send_message(m.chat.id, f"• 𝖮𝗒𝗎𝗇 𝖡𝗂𝗍𝗍𝗂𝗋𝗂𝗅𝖽𝗂 ! \n• **{m.from_user.mention}** \n𝖸𝖾𝗇𝗂 𝖮𝗒𝗎𝗇 𝖡𝖺𝗌𝗅𝖺𝗍𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 \n /game 𝖸𝖺𝗓𝖺𝖻𝗂𝗅𝗂𝗋𝗌𝗂𝗇𝗂𝗓 . . .\n\n𝖮𝗒𝗎𝗇𝖼𝗎 = 𝖯𝗎𝖺𝗇 \n{text_siralama}")
+    await c.send_message(m.chat.id, f"• 𝖮𝗒𝗎𝗇 𝖡𝗂𝗍𝗍𝗂𝗋𝗂𝗅𝖽𝗂 ! \n• **{m.from_user.mention}** \n𝖸𝖾𝗇𝗂 𝖮𝗒𝗎𝗇 𝖡𝖺𝗌𝗅𝖺𝗍𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 \n /game 𝖸𝖺𝗓𝖺𝖻𝗂𝗅𝗂𝗋𝗌𝗂𝗇𝗂𝗓 . . .\n\n𝖮𝗒𝗎𝗇𝖼𝗎 = 𝖯𝗎𝖺𝗇 \n{siralama_text}")
     oyun[m.chat.id] = {}
     
