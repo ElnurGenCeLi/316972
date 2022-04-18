0a5d66d675cda60cbc36456e5303df480c5d756f
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-04-18 15:24:21 +0300

    Update buton.py

--- kelime_bot/plugins/buton.py
+++ kelime_bot/plugins/buton.py
@@ -5,14 +5,14 @@ from pyrogram.types import InlineKeyboardButton
 
 keyboard = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton("🎉  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  🎉", url=f"http://t.me/Shark_Game_Bot?startgroup=new")
+        InlineKeyboardButton("🎉  𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾  🎉", url=f"http://t.me/KelimeUretBot?startgroup=new")
     ],
     [
-        InlineKeyboardButton("🇹🇷  𝖮𝗐𝗇𝖾𝗋 ", url="t.me/ByWolk"),
-        InlineKeyboardButton("✏️  𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 ", url="t.me/Starbotkanal/62"),
+        InlineKeyboardButton("🇹🇷  𝖮𝗐𝗇𝖾𝗋 ", url="t.me/Kakkurt47"),
+        InlineKeyboardButton("✏️  𝖪𝖺𝗇𝖺𝗅 ", url="t.me/tutkusohbet"),
     ],
     [
-        InlineKeyboardButton("✍🏻  𝖮𝗒𝗎𝗇 𝖦𝗋𝗎𝖻𝗎  ✍🏻", url="t.me/SharkGame_TR")
+        InlineKeyboardButton("✍🏻  𝖮𝗒𝗎𝗇 𝖦𝗋𝗎𝖻𝗎  ✍🏻", url="t.me/tutkusohbet")
     ]
 ])
 
