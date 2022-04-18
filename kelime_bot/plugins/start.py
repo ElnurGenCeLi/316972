8c1ed53e3a43e3ab93b0713287a1badd1fc81f3d
HerlockApiniz <96447407+HerlockApiniz@users.noreply.github.com>
2022-04-19 03:57:54 +0400

    Update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -17,9 +17,9 @@ async def kelimeoyun(c:Client, m:Message):
         aktif = False
 
     if aktif:
-        await m.reply("• 𝖹𝖺𝗍𝖾𝗇 𝗌𝗎 𝖺𝗇𝖽𝖺 𝖽𝖾𝗏𝖺𝗆 𝖾𝖽𝖾𝗇 𝖻𝗂𝗋 𝗈𝗒𝗎𝗇 𝗏𝖺𝗋 ✍🏻 \n• 𝖮𝗒𝗎𝗇𝗎 𝖽𝗎𝗋𝖽𝗎𝗋𝗆𝖺𝗄 𝗂𝖼𝗂𝗇 /stop 𝗄𝗈𝗆𝗎𝗍𝗎𝗇𝗎 𝗄𝗎𝗅𝗅𝖺𝗇𝗂𝗇 ✓")
+        await m.reply("**❗ Onsuzda Qrupunuzda Oyun Davam Edir ✍🏻 \n Oyunu Dayandırmaq üçün /stop yaza bilərsiz")
     else:
-        await m.reply(f"**{m.from_user.mention}** 𝖳𝖺𝗋𝖺𝖿𝗂𝗇𝖽𝖺𝗇 ! \n𝖪𝖾𝗅𝗂𝗆𝖾 𝖮𝗒𝗎𝗇𝗎 𝖡𝖺𝗌𝗅𝖺𝗍𝗂𝗅𝖽𝗂 .\n\n🥳 𝖪𝖾𝗒𝗂𝖿𝗅𝗂 𝖮𝗒𝗎𝗇𝗅𝖺𝗋 !", reply_markup=kanal)
+        await m.reply(f"**{m.from_user.mention}** Tərəfindən! \nSöz Tapmaca Oyunu Başladı .\n\nUğurlar !", reply_markup=kanal)
         
         oyun[m.chat.id] = {"kelime":kelime_sec()}
         oyun[m.chat.id]["aktif"] = True
@@ -35,13 +35,13 @@ async def kelimeoyun(c:Client, m:Message):
             kelime_list+= harf + " "
         
         text = f"""
-🎯 𝖱𝖺𝗎𝗇𝖽 : {oyun[m.chat.id]['round']}/60 
-📝 𝖪𝖾𝗅𝗂𝗆𝖾 :   <code>{kelime_list}</code>
-💰 𝖪𝖺𝗓𝖺𝗇𝖽𝗂𝗋𝖺𝖼𝖺𝗄 𝖯𝗎𝖺𝗇 : 1
-🔎 İ𝗉𝗎𝖼𝗎 : 1. {oyun[m.chat.id]["kelime"][0]}
-✍🏻 𝖴𝗓𝗎𝗇𝗅𝗎𝗄 : {int(len(kelime_list)/2)} 
+🎯 Raund : {oyun[m.chat.id]['round']}/60 
+📝 Söz :   <code>{kelime_list}</code>
+💰 Qazanağınız Bal: 1
+🔎 İpucu: 1. {oyun[m.chat.id]["kelime"][0]}
+✍🏻 Uzunluq : {int(len(kelime_list)/2)} 
 
-✏️ 𝖪𝖺𝗋𝗂𝗌𝗂𝗄 𝖧𝖺𝗋𝖿𝗅𝖾𝗋𝖽𝖾𝗇 𝖣𝗈𝗀𝗋𝗎 𝖪𝖾𝗅𝗂𝗆𝖾𝗒𝗂 𝖡𝗎𝗅𝗎𝗇 🥳 🥳 🥳
+✏️ Qarışıq Hərflərdən Düzgün Sözü Tapın 
         """
         await c.send_message(m.chat.id, text)
         
