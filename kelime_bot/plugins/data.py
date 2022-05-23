548c3953790ea9dc821afb2566e28d6d5018ad11
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-05-23 21:50:33 +0300

    Update data.py

--- kelime_bot/plugins/data.py
+++ kelime_bot/plugins/data.py
@@ -2,7 +2,7 @@ from kelime_bot import oyun, rating
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.command("data") & filters.user("wiperowner")
+@Client.on_message(filters.command("data") & filters.user("mahoaga")
 async def data(c:Client, m):
     await m.reply(oyun)
     await m.reply(rating)
