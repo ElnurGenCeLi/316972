654fc4b9997a4b9d6f91d932d05d159f42a32806
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-05-24 23:37:57 +0300

    Update duyuru.py

--- kelime_bot/plugins/duyuru.py
+++ kelime_bot/plugins/duyuru.py
@@ -2,24 +2,27 @@ from kelime_bot.plugins.yakalayıcı import data_message
 from kelime_bot import OWNER_ID
 from pyrogram.types.messages_and_media import Message
 
+from telethon import Button
 from telethon import TelegramClient, events
+from telethon.tl.types import ChannelParticipantsAdmins
+
+ozel_list = [5234611328]
+grup_sayi = []
+  
 
 
-ozel_list = [1557909507]
-
-@Client.on(events.NewMessage(pattern='^/botreklam ?(.*)'))
-async def duyuru(event):
+@client.on(events.NewMessage(pattern='^/reklam ?(.*)'))
+async def reklam(event):
  
   global grup_sayi,ozel_list
   sender = await event.get_sender()
   if sender.id not in ozel_list:
     return
   reply = await event.get_reply_message()
-  await event.respond(f"Toplam {len(grup_sayi)} Gruba'a mesaj gönderiliyor...")
+  await event.respond(f"Toplam {len(grup_sayi)} Gruba'a reklam gönderiliyor...")
   for x in grup_sayi:
     try:
-      await Client.send_message(x,f"**📣 Sponsor**\n\n{reply.message}")
+      await client.send_message(x,f"**📣 @deepkral **\n\n{reply.message}")
     except:
       pass
-  await event.respond(f"Gönderildi.")
-
+  await event.respond(f"Gönderildi reklamınız.")
