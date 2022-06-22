3f0eef46930f73aa6530e3048608e0395c12fbc6
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2022-06-23 01:31:41 +0400

    update stop.py

--- kelime_bot/plugins/stop.py
+++ kelime_bot/plugins/stop.py
@@ -15,7 +15,7 @@ async def stop(c:Client, m:Message):
     
     siralama = []
     for i in oyun[m.chat.id]["oyuncular"]:
-        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} Bal")
+        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} Xal🌟")
     siralama.sort(reverse=True)
     siralama_text = ""
     for i in siralama:
