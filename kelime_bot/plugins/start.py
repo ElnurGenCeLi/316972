e9ef6c892e6d3071bdae7a9fcd9af8d5c7f2352d
Elnur <104248864+ElnurGenCeLi@users.noreply.github.com>
2022-06-23 01:28:20 +0400

    update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -73,10 +73,10 @@ async def kelimeoyun(c:Client, m:Message):
             kelime_list+= harf + " "
         
         text = f"""
-🎯 Raund : {oyun[m.chat.id]['round']}/60 
-📝 Söz :   <code>{kelime_list}</code>
-💰 Qazandığın Xal : 1
-🔎 Kömək : 1. {oyun[m.chat.id]["kelime"][0]}
+🎯 Raund: {oyun[m.chat.id]['round']}/60 
+📝 Söz:   <code>{kelime_list}</code>
+💰 Qazandığın Xal: 1
+🔎 Kömək: {oyun[m.chat.id]["kelime"][0]}
 ✍🏻 Uzunluq : {int(len(kelime_list)/2)} 
 
 ✏️ Qarışık həriflərdən düzgün sözü tapın
