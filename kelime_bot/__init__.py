b52e1b071b66ffb73a86ef8eba0d54798b547f5c
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-07-18 10:21:31 +0400

    Update __init__.py

--- kelime_bot/__init__.py
+++ kelime_bot/__init__.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger(__name__)
 # Hesap
 API_ID = "19832689"
 API_HASH = "a35f2c0d6c4d25456cd01dbe3547f5de"
-TOKEN = "5567094593:AAH_kMdjswMJVQBWGwHyrY2_qDQ-caSvzlk" 
+TOKEN = "5566797987:AAEUDwtAK8mNt_KL2MMu8cCS3O1Um6okoGU" 
 USERNAME = "SozTapmacaRobot"
 
 
@@ -47,5 +47,5 @@ rating = {}
 
 # !!!!!!!!!!!!!! DEĞİŞTİR KESİNLİKLE !!!!!!!!!!!!!!!!
 #      SAHİBİN USER ID'Sİ
-OWNER_ID = 5189923876
+OWNER_ID = 5387158067
 
