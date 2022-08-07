940ac1ef1e2b81eedb1e7ef2c34bfbf315780d92
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-08-07 20:08:25 +0400

    Add files via upload

---
+++ start.py
@@ -0,0 +1,89 @@
+from pyrogram import Client
+from pyrogram import filters
+from random import shuffle
+from pyrogram.types import Message
+from kelime_bot import oyun
+from kelime_bot.helpers.kelimeler import *
+from kelime_bot.helpers.keyboards import *
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+
+keyboard = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton("➕ Məni Qrupa Əlavə Et ➕", url=f"http://t.me/SozTapmacaRobot?startgroup=new")
+    ],
+    [
+        InlineKeyboardButton("🥷 Qurucu", url="https://t.me/vusaliw"),
+        InlineKeyboardButton("📲 Yeniliklər", url="https://t.me/nyzmibotlarim"),
+    ]
+])
+
+
+START = """
+**Salam ! 
+Mənimlə oyun oynamaq istəyirsənsə, bir qrupa at və oyuna başla. Mən özəl söhbətlər üçün yaradılmamışam.
+Rəsmi Kanal @NyzmiBotlar 
+Əlaqə @Vusalim
+"""
+
+HELP = """
+⛑ Əmirlər Menyusu ⛑
+
+/start - Botu başladar
+/basla - Oyunu başlat 
+/skip - Oyunu keç 
+/global - Global reytinq 
+/cancel - Oyunu dayndırmağ 
+
+Əlaqə - @Vusalim 🖤
+"""
+
+# Komutlar. 
+@Client.on_message(filters.command("start"))
+async def start(bot, message):
+  await message.reply_photo("https://te.legra.ph/file/44b32dcc5e13ec5308e9c.jpg",caption=START,reply_markup=keyboard)
+
+@Client.on_message(filters.command("help"))
+async def help(bot, message):
+  await message.reply_photo("https://te.legra.ph/file/44b32dcc5e13ec5308e9c.jpg",caption=HELP) 
+
+# Oyunu başlat. 
+@Client.on_message(filters.command("basla")) 
+async def kelimeoyun(c:Client, m:Message):
+    global oyun
+    aktif = False
+    try:
+        aktif = oyun[m.chat.id]["aktif"]
+        aktif = True
+    except:
+        aktif = False
+
+    if aktif:
+        await m.reply("**Oyun Qurupunuzda Dəvam Edir ✍🏻\nOyunu diyandırmaq üçün /cancel yazıb diyandıra bilərsiz")
+    else:
+        await m.reply(f"**{m.from_user.mention}** Tərəfindən!\nSöz Tapmaq Oyunu Başladı .\n\nYaxşı Olan Qazansın !", reply_markup=kanal)
+        
+        oyun[m.chat.id] = {"kelime":kelime_sec()}
+        oyun[m.chat.id]["aktif"] = True
+        oyun[m.chat.id]["round"] = 1
+        oyun[m.chat.id]["kec"] = 0
+        oyun[m.chat.id]["oyuncular"] = {}
+        
+        kelime_list = ""
+        kelime = list(oyun[m.chat.id]['kelime'])
+        shuffle(kelime)
+        
+        for harf in kelime:
+            kelime_list+= harf + " "
+        
+        text = f"""
+🎯 Raund: {oyun[m.chat.id]['round']}/60 
+📝 Söz:   <code>{kelime_list}</code>
+💰 Qazandığın Xal: 1
+🔎 Kömək: {oyun[m.chat.id]["kelime"][0]}
+✍🏻 Uzunluq : {int(len(kelime_list)/2)} 
+
+✏️ Qarışık həriflərdən düzgün sözü tapın
+        """
+        await c.send_message(m.chat.id, text)
